29bb6efcd0fe11ee9fc5a7ddcee876e53503eefa
Rory Davidson <rda@ihtsdo.org>
2014-04-15 10:57:49 +0200

    Fixed plugins page

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,33 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+var xhr;
+if (window.XMLHttpRequest) xhr = new XMLHttpRequest();      // all browsers except IE
+else xhr = new ActiveXObject("Microsoft.XMLHTTP");      // for IE
+ 
+xhr.open('GET', 'inc/plugins.json', false);
+xhr.onreadystatechange = function () {
+    if (xhr.readyState===4 && xhr.status===200) {           
+        var items = JSON.parse(xhr.responseText);
+        var output = '';
+        for (var key in items) {
+        	if (items[key].section==='overview')
+        	{
+            	output += '<h3>' + items[key].title + '</h3> <p class=lead>' + items[key].paragraph + '</p>';
+        	}
         }
+        document.getElementById('overview').innerHTML = output;
+		
+        var items = JSON.parse(xhr.responseText);
+        var output = '';
+    	
+        for (var key in items) {
+        	if (items[key].section==='modules')
+        	{
+      			output+= '<div class="row">';
+            	output+= '<div class=col-md-3><p class=lead><b>' + items[key].title + '</b></p></div>'
+            	output+= '<div class=col-md-9><p class=lead>' + items[key].paragraph + '</p></div>'
+      			output+= '</div><hr>';
+      		}
+        }
+        document.getElementById('modules').innerHTML = output;
     }
-}());
-
-// Place any jQuery/helper plugins in here.
+}
+xhr.send();
